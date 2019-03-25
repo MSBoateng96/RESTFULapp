7b1b956a5cb5478d497af93390c1b389cd2f616b
Michael Sarpong-Boateng <msarpongboateng@gmail.com>
2019-03-25 15:26:15 +0000

    Added files from Restful App

---
+++ myapp.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, jsonify
+import plotly.graph_objs as go
+from plotly.utils import PlotlyJSONEncoder
+import json
+import requests
+import  requests_cache
+
+requests_cache.install_cache('crime_api_cache', backend='sqlite', expire_after=36000)
+
+app = Flask(__name__)
+
+
+stops_url_template = 'https://data.police.uk/api/stops-street?lat={lat}&lng={lng}&date={data}'
+#categories_url_template = 'https://data.police.uk/api/crime-categories?date={date}'
+
+@app.route('/stopandsearch',  methods=['GET'])
+def stopschart():
+    my_latitude = request.args.get('lat','52.629729')
+    my_longitude = request.args.get('lng','-1.131592')
+    my_date = request.args.get('date','2018-06')
+
+
+    stops_url = stops_url_template.format(lat = my_latitude, lng = my_longitude, data = my_date)
+
+    resp = requests.get(stops_url)
+    if resp.ok:
+        stops = resp.json()
+    else:
+        print(resp.reason)
+
+    search_outcome_stats = {'None': 0}
+    for stop in stops:
+        outcome = stop["outcome"]
+        if not outcome:
+            search_outcome_stats['None'] += 1
+        elif outcome not in search_outcome_stats.keys():
+            search_outcome_stats.update({outcome:1})
+        else:
+            search_outcome_stats[outcome] += 1
+
+    search_object_stats = {'None': 0}
+    for stop in stops:
+        object_of_search = stop["object_of_search"]
+        if not object_of_search:
+            search_object_stats['None'] += 1
+        elif object_of_search not in search_object_stats.keys():
+            search_object_stats.update({object_of_search:1})
+        else:
+            search_object_stats[object_of_search] += 1
+
+    search_ethnicity_stats = {'None': 0}
+    for stop in stops:
+        ethnicity = stop["self_defined_ethnicity"]
+        if not ethnicity:
+            search_ethnicity_stats['None'] += 1
+        elif ethnicity not in search_ethnicity_stats.keys():
+            search_ethnicity_stats.update({ethnicity:1})
+        else:
+            search_ethnicity_stats[ethnicity] += 1
+
+    graphs = [
+            dict(
+                data=[
+                    dict(
+                        values=list(search_ethnicity_stats.values()),
+                        labels=list(search_ethnicity_stats.keys()),
+                        hole=.4,
+                        type='pie',
+                        name='Ethnicity'
+                    ),
+                ],
+                layout=dict(
+                    title='Stop & Search By Ethnicity Stats During {}'.format(my_date)
+                )
+            ),
+            dict(
+                data=[
+                    dict(
+                        values=list(search_object_stats.values()),
+                        labels=list(search_object_stats.keys()),
+                        hole=.4,
+                        type='pie',
+                        name='Object'
+                    ),
+                ],
+                layout=dict(
+                    title='Object of Search Stats During {}'.format(my_date)
+                )
+            ),
+            dict(
+                data=[
+                    dict(
+                        values=list(search_outcome_stats.values()),
+                        labels=list(search_outcome_stats.keys()),
+                        hole=.4,
+                        type='pie',
+                        name='Outcome'
+                    ),
+                ],
+                layout=dict(
+                    title='Outcome of Search Stats During {}'.format(my_date)
+                )
+            ),
+        ]
+
+    ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+
+    graphJSON = json.dumps(graphs, cls=PlotlyJSONEncoder)
+    return render_template('plotholder.html',ids=ids,graphJSON=graphJSON)
+
+
+
+
+
+if __name__=="__main__":
+    app.run(port=8080, debug=True)
